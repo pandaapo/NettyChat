822f2f7a4b7ccd593122fcdd1ae2b91c9b69be05
pandaapo <1052156701@qq.com>
2020-05-10 15:33:17 +0800

    基于Netty的网页版聊天（支持控制台聊天）

---
+++ src/main/java/com/panda/client/ChatClient.java
@@ -0,0 +1,56 @@
+package com.panda.client;
+
+import com.panda.client.handler.ChatClientHandler;
+import com.panda.protocol.IMDecoder;
+import com.panda.protocol.IMEncoder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class ChatClient {
+    private ChatClientHandler clientHandler;
+    private String host;
+    private int port;
+
+    public ChatClient(String nickName) {
+        this.clientHandler = new ChatClientHandler(nickName);
+    }
+
+    public void connect(String host, int port) {
+        this.host = host;
+        this.port = port;
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.SO_KEEPALIVE, true);
+            b.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new IMDecoder());
+                    ch.pipeline().addLast(new IMEncoder());
+                    ch.pipeline().addLast(clientHandler);
+                }
+            });
+            ChannelFuture f = b.connect(this.host, this.port).sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChatClient("Tom").connect("127.0.0.1", 10080);
+        String url = "http://localhost:8080/images/a.png";
+        System.out.println(url.toLowerCase().matches(".*\\.(gif|png|jpg)$"));
+
+    }
+}
---
+++ src/main/java/com/panda/server/handler/TerminalServerHandler.java
@@ -0,0 +1,30 @@
+package com.panda.server.handler;
+
+import com.panda.processor.MsgProcessor;
+import com.panda.protocol.IMMessage;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.concurrent.EventExecutorGroup;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TerminalServerHandler extends SimpleChannelInboundHandler<IMMessage> {
+
+    private MsgProcessor processor = new MsgProcessor();
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, IMMessage msg) throws Exception {
+        processor.sendMsg(ctx.channel(), msg);
+    }
+
+    /**
+     * 异常处理
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.info("Socket Client：与客户端断开连接：" + cause.getMessage());
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
